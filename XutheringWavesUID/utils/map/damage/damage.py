a3a4fd2fae8d4e75ab2e28cfc90862549c631213
Loping151 <wangkailing151@gmail.com>
2025-12-26 19:39:01 +0800

    ⭐健壮！

--- XutheringWavesUID/utils/map/damage/damage.py
+++ XutheringWavesUID/utils/map/damage/damage.py
@@ -1,10 +1,18 @@
-def check_if_ph_3(*args, **kwargs) -> bool:
-    from ..waves_build.damage import check_if_ph_3 as _func
+from gsuid_core.logger import logger
 
-    return _func(*args, **kwargs)
+def check_if_ph_3(*args, **kwargs) -> bool:
+    try:
+        from ..waves_build.damage import check_if_ph_3 as _func
+        return _func(*args, **kwargs)
+    except ImportError:
+        logger.error("请等待下载完成")
+        return False
 
 
 def check_if_ph_5(*args, **kwargs) -> bool:
-    from ..waves_build.damage import check_if_ph_5 as _func
-
-    return _func(*args, **kwargs)
+    try:
+        from ..waves_build.damage import check_if_ph_5 as _func
+        return _func(*args, **kwargs)
+    except ImportError:
+        logger.error("请等待下载完成")
+        return False
--- XutheringWavesUID/utils/safety.py
+++ XutheringWavesUID/utils/safety.py
@@ -1,10 +1,18 @@
-def generate_dynamic_version(*args, **kwargs):
-    from .waves_build.safety import generate_dynamic_version as _func
+from gsuid_core.logger import logger
 
-    return _func(*args, **kwargs)
+def generate_dynamic_version(*args, **kwargs):
+    try:
+        from .waves_build.safety import generate_dynamic_version as _func
+        return _func(*args, **kwargs)
+    except ImportError:
+        logger.error("请等待下载完成")
+        return ""
 
 
 def safe_calc_damage(*args, **kwargs):
-    from .waves_build.safety import safe_calc_damage as _func
-
-    return _func(*args, **kwargs)
+    try:
+        from .waves_build.safety import safe_calc_damage as _func
+        return _func(*args, **kwargs)
+    except ImportError:
+        logger.error("请等待下载完成")
+        return "0", "0"
